2c72b7559335eebfb04582ea36bd5839d628f943
Smit Shah <smitshah24.ss@gmail.com>
2025-07-26 19:13:41 -0400

    fixes env mocks

--- tests/test_job_source.py
+++ tests/test_job_source.py
@@ -14,15 +14,18 @@ source_table_validations = [
     ('schema2.table1', 'col1', False),
 ]
 
+@patch('utils.job_profile.default_source_connection_string', return_value = 'mock_default_conn')
+@patch('utils.helpers.get_env', return_value={'my_data_source_source_conn': 'mock_connection_string'})
 @patch('utils.source_data_accessor.SourceDataAccessor.connect')
 @patch('utils.source_data_accessor.SourceDataAccessor.get_columns_of', 
        return_value = ['col1', 'col2'])
 @patch('utils.source_data_accessor.SourceDataAccessor.get_table_names', 
        return_value = [('db','schema','table1'), ('db','schema','table2')])
 @pytest.mark.parametrize('table,columns,result',source_table_validations)
-def test_source_validation(mock_source, mock_col, mock_table, table, columns, result):
+def test_source_validation(mock_tables, mock_cols, mock_connect, mock_helper_env, mock_default_conn, table, columns, result):
     """"""
     json = valid_full_json.copy()
+    json['source']['datasource'] = 'my_data_source'
     source = Source(json['source'], json['name'])
     source.table_full_name = table
     source.columns = columns
@@ -31,21 +34,19 @@ def test_source_validation(mock_source, mock_col, mock_table, table, columns, re
 
 
 
+@patch('utils.job_profile.default_source_connection_string', return_value = 'mock_default_conn_str')
 @patch('utils.source_data_accessor.SourceDataAccessor.connect',
        side_effect=pyodbc.Error)
 @patch('utils.source_data_accessor.SourceDataAccessor.get_columns_of', 
        side_effect=pyodbc.Error)
 @patch('utils.source_data_accessor.SourceDataAccessor.get_table_names',
        side_effect=pyodbc.Error)
-@patch('utils.env.get_env', return_value = { 
-       'source_conn':  "source_connection_string",
-       'target_sqlite_db': 'target.db',
-       'default_batch_size': 100
-})
 @pytest.mark.parametrize('table,columns,result',source_table_validations)
-def test_source_validation_against_odbc_error(mock_source, mock_col, mock_table,mock_env, table, columns, result):
+def test_source_validation_against_odbc_error(mock_tables, mock_cols, mock_connect, mock_default_conn, table, columns, result):
     """"""
     json = valid_full_json.copy()
+    # This test case does not use the default connection string
+    json['source']['datasource'] = 'my_data_source'
     source = Source(json['source'], json['name'])
     source.table_full_name = table
     source.columns = columns
@@ -54,13 +55,9 @@ def test_source_validation_against_odbc_error(mock_source, mock_col, mock_table,
 
 
 
+@patch('utils.job_profile.default_source_connection_string', return_value = 'mock_default_conn')
 @patch('utils.job_profile.Source.validate_against_source',
        return_value=(True, None))
-@patch('utils.env.get_env', return_value = { 
-       'source_conn':  "source_connection_string",
-       'target_sqlite_db': 'target.db',
-       'default_batch_size': 100
-})
 @pytest.mark.parametrize('invalid_table_name', [
     'my table',
     'mytable;',
@@ -70,14 +67,18 @@ def test_source_validation_against_odbc_error(mock_source, mock_col, mock_table,
     'drop',
     '--' 
 ])
-def test_invalid_source_table_name(_,mock_env, invalid_table_name):
+def test_invalid_source_table_name(mock_validate_source, mock_default_conn, invalid_table_name):
     json = valid_full_json.copy() 
+    # This test case uses the default connection string
+    if 'datasource' in json['source']:
+        del json['source']['datasource']
     source = Source(json['source'], json['name'])
     source.table_full_name = invalid_table_name
     (res, err) = source.validate()
     assert res == False, f'{invalid_table_name} is not a valid source table.'
     
 
+@patch('utils.job_profile.default_source_connection_string', return_value = 'mock_default_conn')
 @patch('utils.job_profile.Source.validate_against_source',
        return_value=(True, None))
 @pytest.mark.parametrize('valid_table_name', [
@@ -85,8 +86,10 @@ def test_invalid_source_table_name(_,mock_env, invalid_table_name):
     'myTable123',
     't.table'
 ])
-def test_valid_source_table_name(_, valid_table_name):
+def test_valid_source_table_name(mock_validate_source, mock_default_conn, valid_table_name):
     json = valid_full_json.copy() 
+    if 'datasource' in json['source']:
+        del json['source']['datasource']
     source = Source(json['source'], json['name'])
     source.table_full_name = valid_table_name
     (res, err) = source.validate()
